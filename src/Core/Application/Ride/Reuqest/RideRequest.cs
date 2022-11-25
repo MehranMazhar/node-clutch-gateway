948078fdaa64c94b5da2eedb61ee10fef919b966
Mehran Mazhar <mehran.mazhar@gmail.com>
2022-11-25 23:31:55 +0330

    riderequest

---
+++ src/Core/Application/Ride/Reuqest/RideRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeClutchGateway.Application.Ride.Reuqest;
+public class RideRequest : IRequest<RideResponse>
+{
+    public double SourceLocation { get; set; }
+    public double DestinationLocation { get; set; }
+    public DateTime Expire { get; set; }
+}
+
+public class RideRequestValidator : CustomValidator<RideRequest>
+{
+    public RideRequestValidator()
+    {
+        RuleFor(p => p.SourceLocation)
+       .NotEmpty();
+
+        RuleFor(p => p.DestinationLocation)
+       .NotEmpty();
+    }
+}
---
+++ src/Host/Controllers/Ride/RideController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using NodeClutchGateway.Application.Ride.Reuqest;
+
+namespace NodeClutchGateway.Host.Controllers.Ride;
+
+public class RideController : VersionNeutralApiController
+{
+    [HttpPost]
+    [OpenApiOperation("Create a ride request by passenger.", "")]
+    public async Task<RideResponse> RideRequest(RideRequest request)
+    {
+        return await Mediator.Send(request);
+    }
+}
+
+public class RideRequestHandler : IRequestHandler<RideRequest, RideResponse>
+{
+
+    public async Task<RideResponse> Handle(RideRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
